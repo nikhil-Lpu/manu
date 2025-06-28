27e85425c4eabbec7c6e8077d630bc93aeb7c1c1
black-devil963 <nikhilkumaraman85@gmail.com>
2025-06-28 15:12:56 +0530

    initial

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,7 +35,6 @@ exports.registerUser = async (req, res) => {
 // ✅ Login user
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
-  res.status(200).json({ message: "Nikhil" });
   try {
     if (!email || !password) {
       return res.status(400).json({ message: "Email and password are required" });
